b89ee88f1e32996d500c5d87330d8b4b09925972
Maximilian Sachs <maximilian.lukas.sachs@outlook.de>
2025-03-27 16:15:41 +0100

    first push - raw website, no buttons

---
+++ QuizBuster/src/App.tsx
@@ -0,0 +1,20 @@
+import quizBusterLogo from './assets/QuizBuster.png'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <a href="https://trello.com/b/h83ALxHx/block-7-team-23" target="_blank">
+          <img src={quizBusterLogo} className="logo quizBuster" alt="Quiz Buster Logo" />
+        </a>
+      </div>
+      <h1>Quiz Buster</h1>
+    </>
+  )
+}
+
+export default App
