62cccc02f129d60ede61b5295a76da45127cfc75
Maximilian Sachs <m.sachs@transact.de>
2025-03-28 14:43:40 +0100

    added Leaderboard.tsx and small fixes

---
+++ QuizBuster/src/pages/Leaderboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Rangliste</h2>
+            <p className="leaderboard">
+                Rangliste Placeholder
+            </p>
+        </div>
+    );
+};
+
+export default About;
+
